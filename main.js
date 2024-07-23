bcd9d651288e5c38fb81e13c4bff2dc50af2b454
azimjonxatamov <azimjonxatamov6@gmail.com>
2024-07-23 12:27:25 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+let name = prompt('ismingizni kiriting')
+let id = prompt('space idenini kiriting')
+let password = prompt('space passwordni kiriting')   
+
+const obj ={
+    name : name,
+    id: id,
+    password: password
+   
+}
+
+
+console.log(obj);
